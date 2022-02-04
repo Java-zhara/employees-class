85027e606bd83b0b24644be1c0cc1da4be659186
Java-zhara <java-zhara@mail.ru>
2022-02-04 22:16:42 +0300

    employees-class

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,10 +1,10 @@
 import './app-info.css'
 
-const AppInfo = ({emploees, increased}) => {
+const AppInfo = ({employees, increased}) => {
   return (
     <div className="app-info">
       <h1>Учет сотрудников в компании GreatCompany</h1>
-      <h2>Общее число сотрудников: {emploees}</h2>
+      <h2>Общее число сотрудников: {employees}</h2>
       <h2>Премию получат: {increased}</h2>
     </div>
   )
